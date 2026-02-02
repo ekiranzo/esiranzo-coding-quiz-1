9ff1a960e24fc9b4243aa1ca94fb2e53c53a37f4
Kalel <esiranzo@up.edu.ph>
2026-02-02 15:36:50 +0800

    init commit

---
+++ esiranzo_inventory.py
@@ -0,0 +1,14 @@
+user_input = str(input(""))
+
+input_lines = user_input.splitlines()
+print(input_lines)
+
+dictItems = { };
+
+# for i in input_lines:
+# 	if i == 0:
+# 		noItems = input_lines[i]
+# 	else:
+# 		item = input_lines[i].split(" ")
+# 		dictItems[item[0]] = item[1]
+# 		print(dictItems)
---
+++ esiranzo_merg.py
@@ -0,0 +1,8 @@
+str1 = str(input(""))
+
+if "69" in str1:
+	print("Mergjad!")
+elif "420" in str1:
+    print("Mergjad!")
+else:
+	print("Leim!")
